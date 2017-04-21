526dda420e20c4f35cb2b744395299aab11c6474
dsolis421 <snapshot8d@gmail.com>
2017-04-20 20:16:02 -0500

    v1 successfully incorporated jquery

--- tobee/src/ShelterSearch.js
+++ tobee/src/ShelterSearch.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import $ from 'jquery';
 
 class ShelterSearch extends Component {
   constructor() {
     super()
 
     this.state = {
-      shelterSearchText: ''
+      shelterSearchText: '',
+      returnedShelters: []
     }
   }
 
@@ -15,6 +18,18 @@ class ShelterSearch extends Component {
     });
   }
 
+  getSheltersByZip(zip) {
+    $.getJSON(`http://api.petfinder.com/shelter.find?location=${zip}&format=json&key=3c73470956892905e562a55f0e113f50&callback=?`)
+    .done(petApiData => {
+      let shelters = petApiData.petfinder.shelters.shelter;
+      console.log(shelters);
+      this.setState({
+        returnedShelters: shelters
+      })
+    });
+    /*.error(err => console.log('getSheltersByZip error ' + err));*/
+  }
+
   render() {
     return (
       <div>
@@ -22,8 +37,11 @@ class ShelterSearch extends Component {
           placeholder="Enter Zip Code"
           maxLength="5"
           onChange={event => this.handleSearchChange(event)}></input>
-        <span id="sheltersearchgo" className="fa fa-search"></span>
+        <span id="sheltersearchgo"
+          className="fa fa-search"
+          onClick={() => this.getSheltersByZip(this.state.shelterSearchText)}></span>
         <span>Powered by <a href="www.petfinder.com" target="blank">Petfinder</a></span>
+        {this.state.returnedShelters.map(shelter => <h3 key={shelter.id.$t}>{shelter.name.$t}</h3>)}
       </div>
     );
   }
